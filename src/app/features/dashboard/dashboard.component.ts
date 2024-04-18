cfa733880ce908886fe59830a98614e0c210e190
Shpileuski Andrei Shpileuski Andrei <a.shpileuski@fincube.uz>
2024-04-18 23:33:35 +0500

    [2.2] Create your first action

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+
+import { Store } from '@ngxs/store';
+
+import { GetTasks } from '../../../state/tasks.state';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private readonly _store = inject(Store);
 
+  public ngOnInit(): void {
+    this.getTasks();
+  }
+
+  private getTasks(): void {
+    this._store.dispatch(new GetTasks());
+  }
 }
--- src/state/tasks.state.ts
+++ src/state/tasks.state.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
-import { State } from '@ngxs/store';
+import { inject, Injectable } from '@angular/core';
+
+import { Observable, tap } from 'rxjs';
+
+import { Action, State, StateContext } from '@ngxs/store';
+
+import { TasksService } from '../app/core/services/requests/tasks.service';
+import { ITask } from '../app/core/interfaces/task.interface';
+import { IResponse } from '../app/core/interfaces/@response.interface';
+
+export class GetTasks {
+  public static readonly type = '[TASKS] Get';
+}
 
 export interface ITasksState {
-  tasks: [];
+  tasks: ITask[];
 }
 
 @Injectable()
@@ -12,4 +23,15 @@ export interface ITasksState {
     tasks: [],
   },
 })
-export class TasksState {}
+export class TasksState {
+  private readonly _tasksService = inject(TasksService);
+
+  @Action(GetTasks)
+  public getTasks(ctx: StateContext<ITasksState>): Observable<GetTasks> {
+    return this._tasksService.getTasks().pipe(
+      tap((res: IResponse<ITask[]>): void => {
+        ctx.setState({ tasks: res.data! });
+      }),
+    );
+  }
+}
